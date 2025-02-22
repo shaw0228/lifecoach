ed6939e374a1c2a88b746d97ebfd4028b0447723
shaw0228 <425928496@qq.com>
2025-02-22 15:32:23 +0800

    �� 修复：Vercel 部署配置

--- api/server.js
+++ api/server.js
@@ -1,11 +1,6 @@
 import { createServer } from 'http';
-import { parse } from 'url';
 import app from '../backend/server.js';
 
-const server = createServer((req, res) => {
-  const parsedUrl = parse(req.url, true);
-  req.query = parsedUrl.query;
+export default function handler(req, res) {
   app(req, res);
-});
-
-export default server;
+}
