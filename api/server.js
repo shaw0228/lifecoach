83cf62feac64e4bd4457ee881b798aed871f1c85
shaw0228 <425928496@qq.com>
2025-02-22 15:35:20 +0800

    �� 更新：Vercel Serverless 函数配置

--- api/server.js
+++ api/server.js
@@ -2,5 +2,11 @@ import { createServer } from 'http';
 import app from '../backend/server.js';
 
 export default function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  
+  // 处理 API 路由
   app(req, res);
 }
